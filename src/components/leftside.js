7431aa82a4fed80011564495f3023d71e70ee781
ibrahim-softsuave <ibrahim.softsuave@gmail.com>
2023-02-28 20:22:00 +0530

    day-3

--- src/components/leftside.js
+++ src/components/leftside.js
@@ -1,49 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './leftside.css' 
 import  * as Aaicons  from "react-icons/ai";
+import { IconContext } from "react-icons/lib";
 
 export const Leftside = () => {
-  return (
-    <div className='element'>
-        <div className='left-element'>
-        <Aaicons.AiOutlineDollar/>
-        <p>Pay Someone</p>
-        <p>you can pay some one in 4 easy steps</p>
-        <form className='form'>
-            <label className='label'>
-                Pay :
-                <input type="text"></input>
-            </label>
-            <label className='label'>
-                Form :
-                <input type="text"></input>
-            </label>
-            <label className='label'>
-                Amount :
-                <input type="text"></input>
-            </label >
-            <label className='label'>
-                Payment Reason :
-                <input type="text"></input>
-            </label>
-            <label className='label'>
-              send advice to :
-                <input type="text"></input>
-            </label>
-            <label className='label'>
-                withholding tax :
-                <input type="text"></input>
-            </label>
-            <label className='label'>
-                supporting documents :
-                <input type="text"></input>
-            </label>
+    const[formData,setFormData]=useState({});
+    const[success,setSuccess]=useState(true);
 
-          
+    const handleChange=(e)=>{
+        const name=e.target.name
+        const value=e.target.value
+        setFormData( values =>({...values,[name]:value}))
+    }
+    const handleNext=(e)=>{
+        e.preventDefault();
+        setSuccess(!success)
+        console.log(formData);
+
+    }
+    const handleBack=()=>{
+        setSuccess(!success)
+    }
+  return (
+    <div className='overAll'>{success?(<>
+    <div className='element'>
+        <div className='left-element'>  
+        <div className='top-elem'>
+            <IconContext.Provider value={{color:'Gold'}}>
+            <p  style={{marginTop:'150px',fontSize:'35px'}}><Aaicons.AiOutlineDollar/></p></IconContext.Provider>
+            <p >Pay Someone</p>
+            <p >you can pay some one in 4 easy steps</p>
+        </div>
+        <form >
+            <label >Pay : </label>
+                <input type="text" name='pay' value={formData.pay} onChange={handleChange}></input><br/>
+            <label >From : </label>
+                <input type="text" name='from' value={formData.from } onChange={handleChange}></input><br/>
+            <label >Amount :</label >
+                <input type="text" name='amount' value={formData.amount} onChange={handleChange}></input><br/>
+            <label >Payment Reason :</label>
+                <input type="text" name='reason' value={formData.reason} onChange={handleChange}></input><br/>
+            <label >send advice to :</label>
+                <input type="text" name='advice' value={formData.advice} onChange={handleChange}></input><br/>
+            <label >withholding tax : </label>
+                <input type="text" name='tax' value={formData.tax} onChange={handleChange}></input><br/>
+            <label className='label'>supporting documents : </label>
+                <input type="text" name='doc' value={formData.doc} onChange={handleChange}></input><br/>
+            <div className='form-footer'>
+            <a href="/payment-form">cancel</a>
+            <button>save as draft</button>
+            <button onClick={handleNext}>next</button>
+            </div>
         </form>
-    
         </div>
         <div className='right-elemnt'>right side</div>
+    </div>
+    </>):(
+    <div className='review'>
+        <div className='review-container'>
+            <p>Review and Continue</p>
+            <p>please review your payment all the details are correct</p>
+            <div className='rectangle'></div>
+            <button onClick={handleBack}>back</button>
+        </div>
+     
+    </div>
+    )
+    }
     </div>
   )
 }
